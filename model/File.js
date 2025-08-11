180ea5d9dbb7de4e931a0bb0b361d40f28b14560
Alok Verma <alokverma0122@gmail.com>
2025-08-11 15:29:17 +0530

    added nodemailer

--- model/File.js
+++ model/File.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -17,6 +18,38 @@ const fileSchema = new mongoose.Schema({
         required:true
     }
 })
+
+//post
+fileSchema.post("save" , async function(doc) {
+    try{
+        console.log("DOC ->",doc);
+
+        let transporter = nodemailer.createTransport({
+            host:process.env.HOST_NAME,
+            auth:{
+                user:process.env.MAIL_USER,
+                pass:process.env.MAIL_PASS,
+            }
+        })
+
+        //send mail
+        let info= await transporter.sendMail({
+            from:"Alok",
+            to:doc.email,
+            subject:"File uploaded successfully",
+            html:`<h2>Hello Jee</h2> <p>File Uploaded View here: < href="${doc.imageUrl}">${doc.imageUrl}</a></p>`
+        })
+        console.log("INFO-> ",info)
+
+    }catch(error){
+        console.error(error);
+        throw error;
+    }
+    
+    
+})
+
+
 
 const File = mongoose.model("file",fileSchema);
 module.exports = File;
